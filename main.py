9784a6476be9a3048525ea4833972198d8c79224
u21h2 <quzhenqing@zju.edu.cn>
2022-05-16 16:55:38 +0800

    update

--- main.py
+++ main.py
@@ -32,14 +32,18 @@ def handler(origin_md_path, output_md_path, image_dir, image_url_prefix, image_r
     with open(origin_md_path, 'r', encoding='utf-8', errors='ignore') as f:
         for line in f.readlines():
             line = re.sub(r'png#(.*)+', 'png)', line)
-            image_url = str(re.findall(r'\[image.png\]\(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',line))
-            if yuque_cdn_domain in image_url and '.png' in image_url:
-                image_url = image_url.replace('[image.png]', '').replace('(', '').replace(')', '').replace('[', '').replace(']', '').replace("'", '')
-                download_image(image_url, image_dir, image_rename_mode, idx)
+            image_url = str(re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',line))
+            if yuque_cdn_domain in image_url:
+                image_url = image_url.replace('(', '').replace(')', '').replace('[', '').replace(']', '').replace("'", '')
+                if '.png' in image_url:
+                    suffix = '.png'
+                elif '.jpeg' in image_url:
+                    suffix = '.jpeg'
+                download_image(image_url, image_dir, image_rename_mode, idx, suffix)
                 to_replace = '/'.join(image_url.split('/')[:-1])
                 new_image_url = image_url.replace(to_replace, 'placeholder')
                 if image_rename_mode == 'asc':
-                    new_image_url = image_url_prefix + image_file_prefix + str(idx) + '.png'
+                    new_image_url = image_url_prefix + image_file_prefix + str(idx) + suffix
                 else:
                     new_image_url = new_image_url.replace('placeholder/',image_url_prefix)
                 idx += 1
@@ -51,11 +55,11 @@ def handler(origin_md_path, output_md_path, image_dir, image_url_prefix, image_r
     return idx
             
 
-def download_image(image_url, image_dir, image_name_mode, idx):
+def download_image(image_url, image_dir, image_name_mode, idx, suffix):
     r = requests.get(image_url, stream=True)
     image_name = image_url.split('/')[-1]
     if image_name_mode == 'asc':
-        image_name = image_file_prefix + str(idx) + '.png'
+        image_name = image_file_prefix + str(idx) + suffix
     if r.status_code == 200:
         open(image_dir+'/'+image_name, 'wb').write(r.content)
     del r
